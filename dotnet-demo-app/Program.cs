5f3792c2ad407f2402f1080d5b1e60b7a1c1ff2b
Rafal Devops <az400raf@outlook.com>
2024-10-06 15:11:07 +0200

    Revert "break it"
    This reverts commit 263ba5aaae36bd14bdf93e09248365a3e568b2a8.

--- dotnet-demo-app/Program.cs
+++ dotnet-demo-app/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Mobilo!");
-Console.WriteLine("The current time is  + DateTime.Now);
+Console.WriteLine("The current time is " + DateTime.Now);
 // https://dotnet.microsoft.com/en-us/learn/dotnet/hello-world-tutorial/intro
